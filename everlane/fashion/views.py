bbac9ece26be98e47c5d6d23c1d6d83d9c0b4d3f
anurk <anuradhakrishnanav2002@gmail.com>
2024-07-30 11:28:52 +0530

    First commit

--- everlane/fashion/views.py
+++ everlane/fashion/views.py
@@ -3,13 +3,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserRegistrationSerializer
 
-<<<<<<< HEAD
-# Create your views here.
-
-def home(request):
-    return render(request, 'home.html')
-
-=======
 class RegisterUserView(generics.CreateAPIView):
     serializer_class = UserRegistrationSerializer
 
@@ -22,4 +15,3 @@ class RegisterUserView(generics.CreateAPIView):
                 "message": "User registered successfully."
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
->>>>>>> bf058002dd74e9957049c46f595a36a81cfb08e8
