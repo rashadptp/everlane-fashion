d7f249301eb22f3046e66fe0749afd48835b9e10
rashadptp <ptprashad@gmail.com>
2024-07-31 15:58:20 +0530

    Model added BAnner

--- everlane/everlane/settings.py
+++ everlane/everlane/settings.py
@@ -135,3 +135,7 @@ REST_FRAMEWORK = {
     ],
   
 }
+import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- everlane/fashion/models.py
+++ everlane/fashion/models.py
@@ -66,6 +66,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     subcategory = models.ForeignKey(Subcategory, related_name='products', on_delete=models.CASCADE)
     image =models.ImageField(upload_to='products/')
+    is_trending=models.BooleanField(default='False')
 
     def __str__(self):
         return self.name
@@ -103,3 +104,13 @@ class CartItem(models.Model):
 
     def __str__(self):
         return f"{self.quantity} of {self.product.name} in {self.cart.user.username}'s cart"
+    
+
+from django.db import models
+class Banner(models.Model): 
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True) 
+    image = models.ImageField(upload_to='banners/') 
+
+    def __str__(self): return self.title  
+
