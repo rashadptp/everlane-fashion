76d82daec78f6b7c17920351c5850fbab960b2e4
rashadptp <ptprashad@gmail.com>
2024-08-02 17:51:35 +0530

    .env created1

--- everlane/everlane/settings.py
+++ everlane/everlane/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -139,7 +141,6 @@ REST_FRAMEWORK = {
     ],
   
 }
-import os
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
