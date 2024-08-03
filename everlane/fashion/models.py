e1a069820499d45ed0526b688d0d30565d54e94e
rashadptp <ptprashad@gmail.com>
2024-08-03 11:11:02 +0530

    Models edits

--- everlane/fashion/models.py
+++ everlane/fashion/models.py
@@ -74,7 +74,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.PositiveIntegerField()
+    stock = models.PositiveIntegerField(null=True)
     subcategory = models.ForeignKey(Subcategory, related_name='products', on_delete=models.CASCADE)
     image =models.ImageField(upload_to='products/',null=True)
     is_trending=models.BooleanField(default='False')
