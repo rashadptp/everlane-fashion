3f0273d32ab418c02b54bcd5f8d0bf17a05e1d22
rashadptp <ptprashad@gmail.com>
2024-08-13 14:46:15 +0530

    Disaster

--- everlane/fashion/serializers.py
+++ everlane/fashion/serializers.py
@@ -166,9 +166,10 @@ class WishlistSerializer(serializers.ModelSerializer):
     product_image = serializers.ImageField(source='product.image', read_only=True) 
     product_name = serializers.ReadOnlyField(source='product.name')
     product_price = serializers.ReadOnlyField(source='product.price')
+    product_desription=serializers.ReadOnlyField(source='product.description')
     class Meta:
         model = Wishlist
-        fields = ['id', 'product','is_active','is_deleted','created_on','product_image','product_name', 'product_price']
+        fields = ['id', 'product','is_active','is_deleted','created_on','product_image','product_name', 'product_price','product_description']
 
 
 class CartItemSerializer(serializers.ModelSerializer):
