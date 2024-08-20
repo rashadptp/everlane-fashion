76ea492bf620a2144a289d1cf72c4af48ecf99cb
rashadptp <ptprashad@gmail.com>
2024-08-20 11:32:18 +0530

    Cart History

--- everlane/fashion/admin.py
+++ everlane/fashion/admin.py
@@ -7,6 +7,8 @@ from .models import *
 
 from django.contrib import admin
 from .models import *
+from django.utils.html import format_html
+import json
 
 admin.site.register(User)
 admin.site.register(Product)
@@ -23,7 +25,19 @@ admin.site.register(Address)
 admin.site.register(Disaster)
 admin.site.register(DressDonation)
 admin.site.register(PickupLocation)
-admin.site.register(CartHistory)
+class CartHistoryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'formatted_cart_data')
+
+    def formatted_cart_data(self, obj):
+        try:
+            cart_data = json.loads(obj.cart_data)
+            return format_html('<pre>{}</pre>', json.dumps(cart_data, indent=4))
+        except json.JSONDecodeError:
+            return 'Invalid JSON'
+    
+    formatted_cart_data.short_description = 'Cart Data'
+
+admin.site.register(CartHistory, CartHistoryAdmin)
 
 
 
