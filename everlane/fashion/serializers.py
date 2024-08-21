42b137d9f4d2c774e3df1fb37d7c042220b1dd45
rashadptp <ptprashad@gmail.com>
2024-08-21 10:31:27 +0530

    AAA

--- everlane/fashion/serializers.py
+++ everlane/fashion/serializers.py
@@ -315,7 +315,7 @@ class DressDonationSerializer(serializers.ModelSerializer):
 
 class DressDonationListSerializer(serializers.ModelSerializer):
     donor_name = serializers.CharField(source='user.username', read_only=True)
-    image = serializers.SerializerMethodField()
+    images = serializers.SerializerMethodField()
     # images = serializers.ListField(
     #     child=serializers.ImageField(max_length=100, allow_empty_file=False, use_url=True),
     #     allow_empty=False,
