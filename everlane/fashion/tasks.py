5ec9487786e7f1f88193a0fffae2f8e82e93ba00
rashadptp <ptprashad@gmail.com>
2024-08-23 11:30:17 +0530

    AA

--- everlane/fashion/tasks.py
+++ everlane/fashion/tasks.py
@@ -1,10 +1,9 @@
-from celery import shared_task
+# from celery import shared_task
 from django.core.mail import send_mail
 from django.conf import settings
 
-@shared_task(bind=True, default_retry_delay=60, max_retries=2)
+# @shared_task(bind=True, default_retry_delay=60, max_retries=2)
 def send_order_status_email(self, order_id, user_email, order_status):
-    print("222222222222222222222222222222222222222222222")
     subject = f'Your order {order_id} status has been updated'
     message = f'The status of your order {order_id} is now: {order_status}.'
     email_from = settings.DEFAULT_FROM_EMAIL
