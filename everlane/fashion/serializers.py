299100d62620cbba33ac196e5a602ec79edd35e0
rashadptp <ptprashad@gmail.com>
2024-08-26 14:48:12 +0530

    AA

--- everlane/fashion/serializers.py
+++ everlane/fashion/serializers.py
@@ -127,10 +127,9 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = ['id', 'name', 'description', 'price','brand', 'subcategory', 'image','is_active','created_on','is_deleted','is_trending','skin_colors','heights','genders','usages']
         
-class RecommendSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Product
-        fields = ['id', 'name', 'description', 'price','brand', 'subcategory', 'image','is_active','created_on','is_deleted','is_trending','winter','summer','rainy','autumn','skin_colors', 'heights', 'genders', 'usages']
+class RecommendSerializer(ProductSerializer):
+    class Meta(ProductSerializer.Meta):
+        fields = ProductSerializer.Meta.fields + ['skin_colors', 'heights', 'genders', 'usages']
 
 class SeosonSerializer(serializers.ModelSerializer):
     class Meta:
